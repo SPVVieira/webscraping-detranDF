646bd7f14d1f921d1ea7bc49c51c54cc30ebde3e
Victor Vieira <victor10jb@hotmail.com>
2021-06-18 10:28:33 -0300

    feat: função para retornar dados do detranDF consulta automática

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-require 
+const puppeteer = require('puppeteer');
+
+async(placa, renavam) => {
+    //Inicial um browser
+    const browser = await puppeteer.launch({
+        //define visibilidade (true = invisível, false = visível)
+        headless: true,
+        //Muta o navegador
+        ignoreDefaultArgs: ['--mute-audio'],
+        //Desativa pop-ups e sandboxes
+        args: ['--disable-notifications', '--no-sandbox', '--disable-setuid-sandbox']
+    });
+    //inicia uma nova aba no navegador
+    const page = await browser.newPage();
+    //redireciona para o site de consultas do detran
+    await page.goto('http://getran.detran.df.gov.br/site/veiculos/consultas/filtroplacarenavam-consultaveiculo.jsp');
+    //aguarda o seletor para iniciar as operações
+    await page.waitForSelector('input#PLACA');
+    //Digita placa e renavam
+    await page.type('input#PLACA', placa);
+    await page.type('input#RENAVAM', renavam);
+    //clica no otão para consultar
+    await page.click('a#CONSULTAR', {
+        button: "left"
+    });
+    //espera um segundo para a página carregar
+    await page.waitForTimeout(1000);
+    //Aguarda o seletor para o "captcha"
+    await page.waitForSelector('label#pergunta');
+    
+
+}
